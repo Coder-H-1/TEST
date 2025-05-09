e53be3bf6752d91c201814e140cefb05f48cf944
Coder H <96431893+Coder-H-1@users.noreply.github.com>
2025-05-09 14:54:36 +0000

    main

---
+++ Python/Powers.py
@@ -0,0 +1,10 @@
+
+
+
+def Powers():
+    Number, Power = input("Enter Number and power | format > Number <space> power : ").split(" ")
+    Number = int(Number)
+    Power = int(Power)
+    print(Number**Power)
+
+Powers()
